7b46d4e6f80d22bcafc6b0001ee2c5a45e94b0df
Torsten Compart <torsten.compart@scout24.com>
2017-04-02 11:22:47 +0200

    Adding online-ausstellung link

--- src/app/navigation/navigationDirective.js
+++ src/app/navigation/navigationDirective.js
@@ -10,6 +10,7 @@ angular.module('nicoco').directive('navigation', [function () {
 				{title: 'Über mich', link: '/aboutme'},
 				{title: 'Angebote', link: '/offer'},
 				{title: 'Blog', link: '/wordpress/', deeplink: true },
+				{title: 'Online-Ausstellung', link: '/wordpress/2017/04/online-ausstellung/', deeplink: true },
 				{title: 'Kunden-Login', link: '/login'}
 			];
 		}
