0df042444be539348c5350aa4326e5c1025ada45
Torsten Compart <torsten.compart@scout24.com>
2017-08-01 22:34:04 +0200

    Removing link to online selling of pictures

--- src/app/navigation/navigationDirective.js
+++ src/app/navigation/navigationDirective.js
@@ -10,7 +10,6 @@ angular.module('nicoco').directive('navigation', [function () {
 				{title: 'Über mich', link: '/aboutme'},
 				{title: 'Angebote', link: '/offer'},
 				{title: 'Blog', link: '/wordpress/', deeplink: true },
-				{title: 'Online-Ausstellung', link: '/wordpress/2017/04/online-ausstellung/', deeplink: true },
 				{title: 'Kunden-Login', link: '/login'}
 			];
 		}
