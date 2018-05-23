52dcc92f21bd7f69dc2f306758b84f573f410900
Torsten Compart <torsten.compart@gmail.com>
2018-05-23 20:54:52 +0200

    correcting image urls and links

--- src/app/carousel/slides.js
+++ src/app/carousel/slides.js
@@ -1,8 +1,8 @@
 angular.module('nicoco').value('slides', [
-	{ src: 'images/slides/1.jpg' },
-	{ src: 'images/slides/2.jpg' },
-	{ src: 'images/slides/3.jpg' },
-	{ src: 'images/slides/4.jpg' },
-	{ src: 'images/slides/5.jpg' },
-	{ src: 'images/slides/6.jpg' }
+	{ src: 'images/slides/101.jpg' },
+	{ src: 'images/slides/102.jpg' },
+	{ src: 'images/slides/103.jpg' },
+	{ src: 'images/slides/104.jpg' },
+	{ src: 'images/slides/105.jpg' },
+	{ src: 'images/slides/106.jpg' }
 ]);
