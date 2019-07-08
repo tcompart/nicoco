1c236278a3e4f7f2b2e19feaa832132ab0a1fa0a
Torsten Compart <torsten.compart@gmail.com>
2019-07-08 23:26:44 +0200

    Fixing github security issues WS-2018-0592 and WS-2018-0107

--- src/app/module.js
+++ src/app/module.js
@@ -1,50 +1,50 @@
 angular.module('nicoco', ['ui.router', 'ngMessages'])
-  .constant('wordpress', 'https://www.nicoco.de/wordpress')
-  .run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
-	$rootScope.$on('$stateChangeSuccess', function () {
-		if ($window.ga) {
-			$window.ga('send', 'pageview', {page: $location.path()});
-		}
-	});
-}])
-  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
-	$locationProvider.html5Mode({
-		enabled: true,
-		requireBase: false
-	});
-	$locationProvider.hashPrefix('!');
+	.constant('wordpress', 'https://www.nicoco.de/wordpress')
+	.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
+		$rootScope.$on('$stateChangeSuccess', function () {
+			if ($window.ga) {
+				$window.ga('send', 'pageview', {page: $location.path()});
+			}
+		});
+	}])
+	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+		$locationProvider.html5Mode({
+			enabled: true,
+			requireBase: false
+		});
+		$locationProvider.hashPrefix('!');
 
-	$urlRouterProvider.otherwise('/home');
+		$urlRouterProvider.otherwise('/home');
 
-	$stateProvider.state({
-		name: 'home',
-		url: '/home',
-		template: ' '
-	});
-	$stateProvider.state({
-		name: 'gallery',
-		url: '/gallery',
-		templateUrl: '/gallery.html',
-		controller: 'galleryCtrl as gallery'
-	});
-	$stateProvider.state({
-		name: 'aboutme',
-		url: '/aboutme',
-		templateUrl: '/aboutme.html'
-	});
-	$stateProvider.state({
-		name: 'offer',
-		url: '/offer',
-		templateUrl: '/offer.html'
-	});
-	$stateProvider.state({
-		name: 'privacy',
-		url: '/privacy',
-		templateUrl: '/privacy.html'
-	});
-	$stateProvider.state({
-		name: 'impressum',
-		url: '/impressum',
-		templateUrl: '/impressum.html'
-	});
-}]);
+		$stateProvider.state({
+			name: 'home',
+			url: '/home',
+			template: ' '
+		});
+		$stateProvider.state({
+			name: 'gallery',
+			url: '/gallery',
+			templateUrl: '/gallery.html',
+			controller: 'galleryCtrl as gallery'
+		});
+		$stateProvider.state({
+			name: 'aboutme',
+			url: '/aboutme',
+			templateUrl: '/aboutme.html'
+		});
+		$stateProvider.state({
+			name: 'offer',
+			url: '/offer',
+			templateUrl: '/offer.html'
+		});
+		$stateProvider.state({
+			name: 'privacy',
+			url: '/privacy',
+			templateUrl: '/privacy.html'
+		});
+		$stateProvider.state({
+			name: 'impressum',
+			url: '/impressum',
+			templateUrl: '/impressum.html'
+		});
+	}]);
